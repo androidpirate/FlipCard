9e30d9d7080ca6f5d33605eb2a2b30f9595356f9
androidpirate <emrekalaycidesign@gmail.com>
2017-10-12 22:25:58 -0400

    NEW FEAT - Added a new feature which automatically hides the soft keyboard during card change.

--- app/src/main/java/com/github/androidpirate/flipcard/FrontCardFragment.java
+++ app/src/main/java/com/github/androidpirate/flipcard/FrontCardFragment.java
@@ -29,6 +29,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -113,6 +114,12 @@ public class FrontCardFragment extends Fragment {
     }
 
     private void checkAnswer(String userInput, String cardRearSide) {
+        View view = getActivity().getCurrentFocus();
+        if(view != null) {
+            InputMethodManager imm = (InputMethodManager) getActivity()
+                    .getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
         if(!userInput.isEmpty()) {
             if(userInput.equalsIgnoreCase(cardRearSide)) {
                 mListener.displayCorrectAnswerAnimation();
