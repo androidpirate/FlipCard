6a4dea11eaaa1b1e3a6a755e3d81d8829f0d2b48
android_pirate <emrekalaycidesign@gmail.com>
2017-10-14 21:43:48 -0400

    NEW FEAT - Added ActionBar menu to DeckListActivity.

--- app/src/main/java/com/github/androidpirate/flipcard/DeckListActivity.java
+++ app/src/main/java/com/github/androidpirate/flipcard/DeckListActivity.java
@@ -3,6 +3,7 @@ package com.github.androidpirate.flipcard;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class DeckListActivity extends SingleFragmentActivity {
@@ -19,11 +20,17 @@ public class DeckListActivity extends SingleFragmentActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_deck_list_menu, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_deck:
+                // Replace DeckListFragment with CreateDeckFragment here
+                return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
