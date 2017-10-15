837e4da7eb9b8f74f46dcfed5f4a0776af3bdbf0
android_pirate <emrekalaycidesign@gmail.com>
2017-10-14 21:33:45 -0400

    NEW FEAT - Added a new activity to display list of decks.

---
+++ app/src/main/java/com/github/androidpirate/flipcard/DeckListActivity.java
@@ -0,0 +1,29 @@
+package com.github.androidpirate.flipcard;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DeckListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
