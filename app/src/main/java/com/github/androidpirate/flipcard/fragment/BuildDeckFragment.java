6c10c7d91712ff7ab7436a515e7848218e73e225
android_pirate <emrekalaycidesign@gmail.com>
2017-10-18 00:16:48 -0400

    REFACTOR - Turned options menu in BuildDeckFragment.

--- app/src/main/java/com/github/androidpirate/flipcard/fragment/BuildDeckFragment.java
+++ app/src/main/java/com/github/androidpirate/flipcard/fragment/BuildDeckFragment.java
@@ -43,7 +43,7 @@ public class BuildDeckFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setHasOptionsMenu(true);
     }
 
     @Override
