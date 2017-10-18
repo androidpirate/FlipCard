f9143fabefa1f775a09c1058dd80310b452fd551
android_pirate <emrekalaycidesign@gmail.com>
2017-10-18 00:37:51 -0400

    DeckBuilderFragment can be accessed from Add action button from DeckActivity.

--- app/src/main/java/com/github/androidpirate/flipcard/DeckActivity.java
+++ app/src/main/java/com/github/androidpirate/flipcard/DeckActivity.java
@@ -1,11 +1,13 @@
 package com.github.androidpirate.flipcard;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.github.androidpirate.flipcard.data.DeckDbHelper;
+import com.github.androidpirate.flipcard.fragment.BuildDeckFragment;
 import com.github.androidpirate.flipcard.fragment.DeckListFragment;
 import com.github.androidpirate.flipcard.model.Deck;
 
@@ -36,6 +38,10 @@ public class DeckActivity extends SingleFragmentActivity
         switch (item.getItemId()) {
             case R.id.add_deck:
                 // Replace DeckListFragment with CreateDeckFragment here
+                FragmentManager fm = getFragmentManager();
+                fm.beginTransaction()
+                        .replace(R.id.fragment_container, BuildDeckFragment.newInstance())
+                        .commit();
                 return true;
         }
         return super.onOptionsItemSelected(item);
