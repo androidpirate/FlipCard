5047ca33625cd465061e7ea1cecda834dee08ee0
androidpirate <emrekalaycidesign@gmail.com>
2017-10-22 21:58:00 -0400

    NEW FEAT - Added MainActivity to be host activity for all fragments.

---
+++ app/src/main/java/com/github/androidpirate/flipcard/MainActivity.java
@@ -0,0 +1,12 @@
+package com.github.androidpirate.flipcard;
+
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        // Add the fragment instance to show up here
+        return null;
+    }
+}
