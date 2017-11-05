a20f44cb6608f15d40da6415997df1d28277e456
android_pirate <emrekalaycidesign@gmail.com>
2017-11-05 18:50:41 -0500

    NEW FEAT - Added a Collapsable Toolbar and header for DeckDetailFragment.

--- app/src/main/java/com/github/androidpirate/flipcard/fragment/DeckDetailFragment.java
+++ app/src/main/java/com/github/androidpirate/flipcard/fragment/DeckDetailFragment.java
@@ -3,15 +3,25 @@ package com.github.androidpirate.flipcard.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.github.androidpirate.flipcard.MainActivity;
 import com.github.androidpirate.flipcard.R;
 import com.github.androidpirate.flipcard.adapter.DeckDetailAdapter;
 import com.github.androidpirate.flipcard.model.Deck;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,9 +35,23 @@ public class DeckDetailFragment extends Fragment
     implements DeckDetailAdapter.OnAdapterInteractionListener {
     private static final String ARG_DECK = "deck";
     private Deck mDeck;
+    private TextView mTitle;
+    private TextView mCategory;
+    private TextView mSize;
     private RecyclerView mRecyclerView;
     private DeckDetailAdapter mAdapter;
     private OnFragmentInteractionListener mListener;
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     */
+    public interface OnFragmentInteractionListener {
+        List<Deck> getDecks();
+        void replaceFragment(Fragment fragment);
+    }
 
     public DeckDetailFragment() {
         // Required empty public constructor
@@ -53,12 +77,27 @@ public class DeckDetailFragment extends Fragment
         if (getArguments() != null) {
             mDeck = (Deck) getArguments().getSerializable(ARG_DECK);
         }
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_deck_detail, container, false);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+
+        mTitle = view.findViewById(R.id.tv_deck_title);
+        mTitle.setText(mDeck.getTitle());
+
+        mCategory = view.findViewById(R.id.tv_deck_category);
+        mCategory.setText(mDeck.getCategory());
+
+        mSize = view.findViewById(R.id.tv_deck_size);
+        mSize.setText(mDeck.getSize() + " Cards");
+
         mRecyclerView = view.findViewById(R.id.rv_deck_detail);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         if(mAdapter == null) {
@@ -68,6 +107,34 @@ public class DeckDetailFragment extends Fragment
         mRecyclerView.setAdapter(mAdapter);
         // Inflate the layout for this fragment
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MainActivity activity = (MainActivity) getActivity();
+        if(activity != null) {
+            activity.showUpButton();
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_deck_detail_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.practice:
+                return true;
+            case android.R.id.home:
+                // Return back to DeckListFragment here
+                ArrayList<Deck> decks = (ArrayList<Deck>) mListener.getDecks();
+                mListener.replaceFragment(DeckListFragment.newInstance(decks));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -85,16 +152,6 @@ public class DeckDetailFragment extends Fragment
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     */
-    public interface OnFragmentInteractionListener {
-        void replaceFragment(Fragment fragment);
     }
 
     @Override
